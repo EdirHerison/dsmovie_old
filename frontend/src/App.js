858e2f5a2eecab01593856e2ce1eedf986c1b51d
EdirHerison <edir_h@hotmail.com>
2022-01-11 14:05:39 -0300

    routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
+import{
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from "./Components/pages/Listing";
+import Form from "./Components/pages/Form";
 import Navbar from "./Components/Navbar";
 
-function App() {
-  return (
+function App(){
+  return(
+    <BrowserRouter>
      <Navbar />
+     <Routes>
+     <Route path="/" element={<Listing />} />
+      <Route path="/form">
+         <Route path=":movieId" element={<Form />} />
+      </Route>
+     </Routes> 
+    </BrowserRouter>
   );
 }
 
